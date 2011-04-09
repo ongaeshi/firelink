374a8b0e469cf6d53463c7d7d88c695074fcf52f
ongaeshi <ongaeshi0621@gmail.com>
2011-04-10 01:22:02 +0900

    設定メニュー作成中、関連ファイルを追加

--- data/setting.js
+++ data/setting.js
@@ -6,15 +6,52 @@
 
 var textArea = document.getElementById('annotation-box');
 
-textArea.onkeyup = function(event) {
-  if (event.keyCode == 13) {
-    postMessage(textArea.value);
-    textArea.value = '';
-  }
-};
+// textArea.onkeyup = function(event) {
+// //   if (event.keyCode == 13) {
+// //     postMessage(textArea.value);
+// //     textArea.value = '';
+// //   }
+// };
 
 self.on('message', function(linkformData) {
-  var textArea = document.getElementById('annotation-box');
-  textArea.value = linkformData[0][2];
-  textArea.focus();
+//   var data = [
+//     {label:"11", format:"12", filter:"13"},
+//     {label:"11", format:"12", filter:"13"},
+//     {label:"11", format:"12", filter:"13"},
+//     {label:"11", format:"12", filter:"13"},
+//   ];
+  
+//   var ctable = new CocoaTable(data, [
+//     'label', 'format', 'filter'
+//   ] );
+  
+  var ctable = new CocoaTable(linkformData, [
+    'name', 'format'
+  ] );
+
+  ctable._listener.onUpdated = function () {
+    //var json = ctable.serialize();
+    //localStorage[localStorageKey] = json;
+  }
+  
+  window.ctable = ctable;
+  
+  
+//   var textArea = document.getElementById('annotation-box');
+//   textArea.value = linkformData[0][2];
+//   textArea.focus();
 });
+
+var ok_button = document.getElementById('okbutton');
+
+ok_button.onclick = function(event) {
+  var defaultLinkformData = [
+    {name:"PlainText",   format:"%text%\n%url%"},
+    {name:"HTML",        format:"<a href=\"%url%\">%text%</a>"},
+    {name:"TiddlyWiki",  format:"[[%text%|%url%]]"},
+    {name:"hatena",      format:"[%url%:title=%text%]"}
+  ];
+  
+  postMessage(defaultLinkformData);
+}
+
