5bd56edb1b573c1b4a9bae38cc210ba05cd25f5b
ongaeshi <ongaeshi0621@gmail.com>
2011-04-10 02:12:51 +0900

    設定メニューとのデータのやり取りを整理

--- data/setting.js
+++ data/setting.js
@@ -4,54 +4,24 @@
 // @author ongaeshi
 // @date   2011/04/07
 
-var textArea = document.getElementById('annotation-box');
-
-// textArea.onkeyup = function(event) {
-// //   if (event.keyCode == 13) {
-// //     postMessage(textArea.value);
-// //     textArea.value = '';
-// //   }
-// };
-
 self.on('message', function(linkformData) {
-//   var data = [
-//     {label:"11", format:"12", filter:"13"},
-//     {label:"11", format:"12", filter:"13"},
-//     {label:"11", format:"12", filter:"13"},
-//     {label:"11", format:"12", filter:"13"},
-//   ];
-  
-//   var ctable = new CocoaTable(data, [
-//     'label', 'format', 'filter'
-//   ] );
-  
-  var ctable = new CocoaTable(linkformData, [
-    'name', 'format'
-  ] );
+  // テーブルを空に
+  $('#formats').empty();
 
-  ctable._listener.onUpdated = function () {
-    //var json = ctable.serialize();
-    //localStorage[localStorageKey] = json;
-  }
-  
-  window.ctable = ctable;
-  
-  
-//   var textArea = document.getElementById('annotation-box');
-//   textArea.value = linkformData[0][2];
-//   textArea.focus();
+  // テーブルを生成
+  var ctable = new CocoaTable(linkformData, ['name', 'format']);
 });
 
-var ok_button = document.getElementById('okbutton');
-
-ok_button.onclick = function(event) {
-  var defaultLinkformData = [
-    {name:"PlainText",   format:"%text%\n%url%"},
+$('#ok_button').bind('click', function(event) {
+  var dummyData = [
+    {name:"PT",          format:"t:%text%\nu:%url%"},
     {name:"HTML",        format:"<a href=\"%url%\">%text%</a>"},
     {name:"TiddlyWiki",  format:"[[%text%|%url%]]"},
     {name:"hatena",      format:"[%url%:title=%text%]"}
   ];
-  
-  postMessage(defaultLinkformData);
-}
+  postMessage(dummyData);
+});
 
+$('#cancel_button').bind('click', function(event) {
+  postMessage(null);
+});
