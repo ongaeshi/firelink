1edf0416acd543aa40782e5ff9fcca3f8cb5f22c
ongaeshi <ongaeshi0621@gmail.com>
2012-08-13 15:33:02 +0900

    [lib] お行儀のいいように
    パネルを一枚で使い回す
    enterキーのハンドリング<form>を使わずjQueryのイベントで

--- data/customize-link.js
+++ data/customize-link.js
@@ -1,5 +1,6 @@
-// ボタンイベント
+// イベント登録
 $(function() {
+  // okボタンを押した
   $("#ok-button").click(function() {
     var msg = {
       kind: 'ok',
@@ -12,12 +13,29 @@ $(function() {
     self.postMessage(msg);
   });
 
+  // enterキーを押下
+  $('body').keypress(function (e) {
+    if ((e.keyCode && e.keyCode == 13)) {
+      var msg = {
+        kind: 'ok',
+        linkdata: {
+          text: $('#text-input').val(),
+          url: $('#url-input').val(),
+          title: $('#title-input').val()
+        }
+      };
+      self.postMessage(msg);
+    }
+  });  
+
+  // canselボタンを押した
   $("#close-button").click(function() {
     var msg = {
       kind: 'close'
     };
     self.postMessage(msg);
   });
+
 });
 
 // Add-on Script からのメッセージ受け取り
