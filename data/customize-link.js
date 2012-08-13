a23f3cf2fcbef036146efe41e76b3a8a76a167fa
ongaeshi <ongaeshi0621@gmail.com>
2012-08-13 17:48:19 +0900

    [lib] 修正 : カスタムリンクパネルが二回目移行書き換わらない
    パネル生成と、イベントの発生を分離

--- data/customize-link.js
+++ data/customize-link.js
@@ -1,41 +1,33 @@
 // イベント登録
 $(function() {
-  // okボタンを押した
-  $("#ok-button").click(function() {
-    var msg = {
+  var okMessage = function() {
+    return {
       kind: 'ok',
+      label: $('h1').html(),
       linkdata: {
         text: $('#text-input').val(),
         url: $('#url-input').val(),
         title: $('#title-input').val()
       }
     };
-    self.postMessage(msg);
+  };
+              
+  // okボタンを押した
+  $("#ok-button").click(function() {
+    self.postMessage(okMessage());
   });
 
   // enterキーを押下
   $('body').keypress(function (e) {
     if ((e.keyCode && e.keyCode == 13)) {
-      var msg = {
-        kind: 'ok',
-        linkdata: {
-          text: $('#text-input').val(),
-          url: $('#url-input').val(),
-          title: $('#title-input').val()
-        }
-      };
-      self.postMessage(msg);
+      self.postMessage(okMessage());
     }
   });  
 
   // canselボタンを押した
   $("#close-button").click(function() {
-    var msg = {
-      kind: 'close'
-    };
-    self.postMessage(msg);
+    self.postMessage({kind: 'close'});
   });
-
 });
 
 // Add-on Script からのメッセージ受け取り
@@ -47,6 +39,7 @@ self.on('message', function(msg) {
     $('#text-input').val(msg.linkdata.text);
     $('#url-input').val(msg.linkdata.url);
     $('#title-input').val(msg.linkdata.title);
+    self.postMessage({kind: 'show'});
     break;
   }
 });
